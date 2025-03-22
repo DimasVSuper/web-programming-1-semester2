2d78d7589b00a0db74bbae7eb66d656318210b74
Dimas Bayu Nugroho <193190491+DimasVSuper@users.noreply.github.com>
2025-03-22 20:04:55 +0700

    tugas webpro 1

---
+++ webtugas/index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>DimasWeb</title>
+</head>
+<body>
+
+    <h1>DimasWeb (19240384)!</h1>
+    <p>Contoh 1</p>
+    <br>
+
+    <!DOCTYPE html>
+<html>
+<head>
+    <title>Daftar Produk</title>
+</head>
+<body>
+
+    <table border="1" width="70%" align="center">
+        <tr>
+            <th colspan="3" bgcolor="orange">DAFTAR PRODUK</th>
+        </tr>
+        <tr>
+            <td rowspan="3" align="center">
+                <img src="pikachu-camera.jpg" width="150">
+            </td>
+            <td bgcolor="green" style="color: white;">Nama Produk</td>
+            <td bgcolor="pink">Canon EOS M10 Kit EF-M 15-45mm</td>
+        </tr>
+        <tr>
+            <td bgcolor="green" style="color: white;">Harga</td>
+            <td bgcolor="pink">Rp 4,899,000</td>
+        </tr>
+        <tr>
+            <td bgcolor="green" style="color: white;">Fitur Produk</td>
+            <td bgcolor="pink">
+                <ul>
+                    <li>Kamera mirrorless</li>
+                    <li>Efektifitas Piksel: 18 MP</li>
+                    <li>Tipe sensor: CMOS DIGIC 6</li>
+                    <li>Layar: 3.0 Inch</li>
+                    <li>Memiliki lampu flash dengan jarak jangkauan hingga 5 meter</li>
+                </ul>
+            </td>
+        </tr>
+    </table>
+
+</body>
+</html>
+</body>
+</html>
