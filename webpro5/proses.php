93272fc3cc051cf0ba02e2af898c7262bbdef8dd
Dimas Bayu Nugroho <193190491+DimasVSuper@users.noreply.github.com>
2025-03-22 17:55:04 +0700

    commit webpro5 changes

--- webpro5/proses.php
+++ webpro5/proses.php
@@ -4,11 +4,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Proses Data</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <h1>DATA ANDA SEDANG DI PROSES</h1>
-    <h2>Nama: <?php echo htmlspecialchars($_POST['nama']); ?></h2>
-    <h2>Email: <?php echo htmlspecialchars($_POST['email']); ?></h2>
+    <main class="form">
+    <h1>DATA ANDA SEDANG DI PROSES</h1> <br>
+    <h2>Nama: <?php echo htmlspecialchars($_POST['nama']); ?></h2> 
+    <h2>Email: <?php echo htmlspecialchars($_POST['email']); ?></h2> 
     <h2>Keluhan: <?php echo nl2br(htmlspecialchars($_POST['keluhan'])); ?></h2>
+    <a href="index.php"><button class="Reinput">Input data lagi</button></a>
+    </main>
 </body>
 </html>
